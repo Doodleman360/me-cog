e24ef05c749eaec38f9fdb1a69bd9e61660bdfd5
Doodleman360 <julislo123@gmail.com>
2016-09-21 19:42:47 -0600

    added DM

---
+++ DM.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+from .utils import checks
+
+class DM:
+    """DM a user."""
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    @checks.admin_or_permissions(manage_server=True)
+    async def DM(self, user : discord.Member, format_msg):
+        """DM a user."""
+        await self.bot.say(format_msg)
+        await self.bot.send_message(member, format_msg)
+
+def setup(bot):
+    bot.add_cog(DM(bot))
