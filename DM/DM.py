b7a00955e0f306f42f1e6434a3663477e341198e
Doodleman360 <julislo123@gmail.com>
2016-09-22 12:56:33 -0600

    added saving

--- DM/DM.py
+++ DM/DM.py
@@ -1,12 +1,35 @@
 import discord
 from discord.ext import commands
+from .utils.dataIO import fileIO
 from .utils import checks
+import os
+
+default_settings = {"AUTODEL": True}
 
 class DM:
     """DM a user."""
 
     def __init__(self, bot):
         self.bot = bot
+        self.settings = fileIO("data/DM/settings.json", "load")
+    
+    @commands.command(pass_context=True)
+    @checks.admin_or_permissions(manage_server=True)
+    async def dmSet(self, ctx, choice : str=None);
+        """Sets whether or not to delete the comand when it is typed
+            
+            Options:
+            off - turns off deleting
+            on - turns on deleting"""
+        options = {"off": False, "on": True}
+        server = ctx.message.server
+        if choice.lower() not in options:
+            await send_cmd_help(ctx)
+            return
+        else:
+            self.settings[server.id]["AUTODEL"] = options[choice.lower()]
+            fileIO("data/welcome/settings.json", "save", self.settings)
+    
 
     @commands.command(pass_context=True)
     @checks.admin_or_permissions(manage_server=True)
@@ -16,5 +39,28 @@ class DM:
         await self.bot.send_message(user, format_msg)
         await self.bot.purge_from(channel, limit=1)
 
+def check_folders():
+    if not os.path.exists("data/DM"):
+        print("Creating data/welcome folder...")
+        os.makedirs("data/DM")
+
+def check_files():
+    f = "data/DM/settings.json"
+    if not fileIO(f, "check"):
+        print("Creating welcome settings.json...")
+        fileIO(f, "save", {})
+    else: #consistency check
+        current = fileIO(f, "load")
+        for k,v in current.items():
+            if v.keys() != default_settings.keys():
+                for key in default_settings.keys():
+                    if key not in v.keys():
+                        current[k][key] = default_settings[key]
+                        print("Adding " + str(key) + " field to welcome settings.json")
+        fileIO(f, "save", current)
+
+
 def setup(bot):
     bot.add_cog(DM(bot))
+    check_folders()
+    check_files()
