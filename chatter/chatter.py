df9c6e4d039758dc98504e0953b3d1d966e02126
Doodleman360 <julislo123@gmail.com>
2016-09-28 14:00:22 -0600

    added chatter cog

---
+++ chatter/chatter.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+from .utils import checks
+from chatterbot import ChatBot
+
+class chatter:
+    """chatter with a user."""
+
+    def __init__(self, bot):
+        self.bot = bot
+        chatbot = ChatBot('Ron Obvious', trainer='chatterbot.trainers.ChatterBotCorpusTrainer')
+        chatbot.train("chatterbot.corpus.english")
+    
+    @commands.command(pass_context=True)
+    async def chat(self, format_msg):
+        await self.bot.say(chatbot.get_response(format_msg))
+
+def setup(bot):
+    bot.add_cog(DM(bot))
+
