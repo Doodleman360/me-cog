fdedd75804366998e00363b94c2725f34e2444e9
Doodleman360 <julislo123@gmail.com>
2016-09-28 18:20:29 -0600

    moved them more (i dont do python)

--- chatter/chatter.py
+++ chatter/chatter.py
@@ -5,9 +5,6 @@ from .utils import checks
 
 class chatter:
     """chatter with a user."""
-    
-    chatbot = ChatBot('Ron Obvious', trainer='chatterbot.trainers.ChatterBotCorpusTrainer')
-    chatbot.train("chatterbot.corpus.english")
 
     def __init__(self, bot):
         self.bot = bot
@@ -15,6 +12,8 @@ class chatter:
     @commands.command(pass_context=True)
     async def chat(self, format_msg):
         """chat"""
+        chatbot = ChatBot('Ron Obvious', trainer='chatterbot.trainers.ChatterBotCorpusTrainer')
+        chatbot.train("chatterbot.corpus.english")
         await self.bot.say(chatbot.get_response(format_msg))
 
 def setup(bot):
