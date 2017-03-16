328f2583e17eb568cd66e0dd65fa92eb22a4e1aa
Doodleman360 <julislo123@gmail.com>
2017-03-15 22:14:35 -0600

    i dont know how to python

--- rnsfw/rnsfw.py
+++ rnsfw/rnsfw.py
@@ -35,133 +35,173 @@ class rnsfw:
             await self.bot.say(":x: **Error:** `{}`".format(e))
 
     @rnsfw.command(no_pm=True)
-    async def konachan(self):
+    async def konachan(self, i):
         """Random Image From Konachan"""
+        num = int(i)
+        if num > 11:
+            num = 10
         try:
-            query = ("https://konachan.com/post/random")
-            page = await aiohttp.get(query)
-            page = await page.text()
-            soup = BeautifulSoup(page, 'html.parser')
-            image = soup.find(id="highres").get("href")
-            await self.bot.say(image)
+            for rand in range(0,num):
+                query = ("https://konachan.com/post/random")
+                page = await aiohttp.get(query)
+                page = await page.text()
+                soup = BeautifulSoup(page, 'html.parser')
+                image = soup.find(id="highres").get("href")
+                await self.bot.say(image)
         except Exception as e:
             await self.bot.say(":x: **Error:** `{}`".format(e))
 
     @rnsfw.command(no_pm=True)
-    async def e621(self):
+    async def e621(self, i):
         """Random Image From e621"""
+        num = int(i)
+        if num > 11:
+            num = 10
         try:
-            query = ("https://e621.net/post/random")
-            page = await aiohttp.get(query)
-            page = await page.text()
-            soup = BeautifulSoup(page, 'html.parser')
-            image = soup.find(id="highres").get("href")
-            await self.bot.say(image)
+            for rand in range(0,num):
+                query = ("https://e621.net/post/random")
+                page = await aiohttp.get(query)
+                page = await page.text()
+                soup = BeautifulSoup(page, 'html.parser')
+                image = soup.find(id="highres").get("href")
+                await self.bot.say(image)
         except Exception as e:
             await self.bot.say(":x: **Error:** `{}`".format(e))
 
     @rnsfw.command(no_pm=True)
-    async def rule34(self):
+    async def rule34(self, i):
         """Random Image From rule34"""
+        num = int(i)
+        if num > 11:
+            num = 10
         try:
-            query = ("http://rule34.xxx/index.php?page=post&s=random")
-            page = await aiohttp.get(query)
-            page = await page.text()
-            soup = BeautifulSoup(page, 'html.parser')
-            image = soup.find(id="image").get("src")
-            await self.bot.say('http:' + image)
+            for rand in range(0,num):
+                query = ("http://rule34.xxx/index.php?page=post&s=random")
+                page = await aiohttp.get(query)
+                page = await page.text()
+                soup = BeautifulSoup(page, 'html.parser')
+                image = soup.find(id="image").get("src")
+                await self.bot.say('http:' + image)
         except Exception as e:
             await self.bot.say(":x: **Error:** `{}`".format(e))
 
     @rnsfw.command(no_pm=True)
-    async def danbooru(self):
+    async def danbooru(self, i):
         """Random Image From Danbooru"""
+        num = int(i)
+        if num > 11:
+            num = 10
         try:
-            query = ("http://danbooru.donmai.us/posts/random")
-            page = await aiohttp.get(query)
-            page = await page.text()
-            soup = BeautifulSoup(page, 'html.parser')
-            image = soup.find(id="image").get("src")
-            await self.bot.say('http://danbooru.donmai.us' + image)
+            for rand in range(0,num):
+                query = ("http://danbooru.donmai.us/posts/random")
+                page = await aiohttp.get(query)
+                page = await page.text()
+                soup = BeautifulSoup(page, 'html.parser')
+                image = soup.find(id="image").get("src")
+                await self.bot.say('http://danbooru.donmai.us' + image)
         except Exception as e:
             await self.bot.say(":x: **Error:** `{}`".format(e))
 
     @rnsfw.command(no_pm=True)
-    async def gelbooru(self):
+    async def gelbooru(self, i):
         """Random Image From Gelbooru"""
+        num = int(i)
+        if num > 11:
+            num = 10
         try:
-            query = ("http://www.gelbooru.com/index.php?page=post&s=random")
-            page = await aiohttp.get(query)
-            page = await page.text()
-            soup = BeautifulSoup(page, 'html.parser')
-            image = soup.find(id="image").get("src")
-            await self.bot.say(image)
+            for rand in range(0,num):
+                query = ("http://www.gelbooru.com/index.php?page=post&s=random")
+                page = await aiohttp.get(query)
+                page = await page.text()
+                soup = BeautifulSoup(page, 'html.parser')
+                image = soup.find(id="image").get("src")
+                await self.bot.say(image)
         except Exception as e:
             await self.bot.say(":x: **Error:** `{}`".format(e))
 
     @rnsfw.command(no_pm=True)
-    async def tbib(self):
+    async def tbib(self, i):
         """Random Image From DrunkenPumken"""
+        num = int(i)
+        if num > 11:
+            num = 10
         try:
-            query = ("http://www.tbib.org/index.php?page=post&s=random")
-            page = await aiohttp.get(query)
-            page = await page.text()
-            soup = BeautifulSoup(page, 'html.parser')
-            image = soup.find(id="image").get("src")
-            await self.bot.say("http:" + image)
+            for rand in range(0,num):
+                query = ("http://www.tbib.org/index.php?page=post&s=random")
+                page = await aiohttp.get(query)
+                page = await page.text()
+                soup = BeautifulSoup(page, 'html.parser')
+                image = soup.find(id="image").get("src")
+                await self.bot.say("http:" + image)
         except Exception as e:
             await self.bot.say(":x: **Error:** `{}`".format(e))
 
     @rnsfw.command(no_pm=True)
-    async def xbooru(self):
+    async def xbooru(self, i):
         """Random Image From Xbooru"""
+        num = int(i)
+        if num > 11:
+            num = 10
         try:
-            query = ("http://xbooru.com/index.php?page=post&s=random")
-            page = await aiohttp.get(query)
-            page = await page.text()
-            soup = BeautifulSoup(page, 'html.parser')
-            image = soup.find(id="image").get("src")
-            await self.bot.say(image)
+            for rand in range(0,num):
+                query = ("http://xbooru.com/index.php?page=post&s=random")
+                page = await aiohttp.get(query)
+                page = await page.text()
+                soup = BeautifulSoup(page, 'html.parser')
+                image = soup.find(id="image").get("src")
+                await self.bot.say(image)
         except Exception as e:
             await self.bot.say(":x: **Error:** `{}`".format(e))
 
     @rnsfw.command(no_pm=True)
-    async def furrybooru(self):
+    async def furrybooru(self, i):
         """Random Image From Furrybooru"""
+        num = int(i)
+        if num > 11:
+            num = 10
         try:
-            query = ("http://furry.booru.org/index.php?page=post&s=random")
-            page = await aiohttp.get(query)
-            page = await page.text()
-            soup = BeautifulSoup(page, 'html.parser')
-            image = soup.find(id="image").get("src")
-            await self.bot.say(image)
+            for rand in range(0,num):
+                query = ("http://furry.booru.org/index.php?page=post&s=random")
+                page = await aiohttp.get(query)
+                page = await page.text()
+                soup = BeautifulSoup(page, 'html.parser')
+                image = soup.find(id="image").get("src")
+                await self.bot.say(image)
         except Exception as e:
             await self.bot.say(":x: **Error:** `{}`".format(e))
 
     @rnsfw.command(no_pm=True)
-    async def drunkenpumken(self):
+    async def drunkenpumken(self, i):
         """Random Image From DrunkenPumken"""
+        num = int(i)
+        if num > 11:
+            num = 10
         try:
-            query = ("http://drunkenpumken.booru.org/index.php?page=post&s=random")
-            page = await aiohttp.get(query)
-            page = await page.text()
-            soup = BeautifulSoup(page, 'html.parser')
-            image = soup.find(id="image").get("src")
-            await self.bot.say(image)
+            for rand in range(0,num):
+                query = ("http://drunkenpumken.booru.org/index.php?page=post&s=random")
+                page = await aiohttp.get(query)
+                page = await page.text()
+                soup = BeautifulSoup(page, 'html.parser')
+                image = soup.find(id="image").get("src")
+                await self.bot.say(image)
         except Exception as e:
             await self.bot.say(":x: **Error:** `{}`".format(e))
 
     @rnsfw.command(no_pm=True)
-    async def lolibooru(self):
+    async def lolibooru(self, i):
         """Random Image From Lolibooru"""
+        num = int(i)
+        if num > 11:
+            num = 10
         try:
-            query = ("https://lolibooru.moe/post/random/")
-            page = await aiohttp.get(query)
-            page = await page.text()
-            soup = BeautifulSoup(page, 'html.parser')
-            image = soup.find(id="image").get("src")
-            image = image.replace(' ','%20')
-            await self.bot.say(image)
+            for rand in range(0,num):
+                query = ("https://lolibooru.moe/post/random/")
+                page = await aiohttp.get(query)
+                page = await page.text()
+                soup = BeautifulSoup(page, 'html.parser')
+                image = soup.find(id="image").get("src")
+                image = image.replace(' ','%20')
+                await self.bot.say(image)
         except Exception as e:
             await self.bot.say(":x: **Error:** `{}`".format(e))
 
