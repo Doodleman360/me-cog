03fd61635ec3ecfbe7698670411718bc8bd22744
Doodleman360 <julislo123@gmail.com>
2017-03-17 19:59:11 -0600

    testing

--- reddit/reddit.py
+++ reddit/reddit.py
@@ -14,18 +14,14 @@ class reddit:
     @checks.admin_or_permissions(manage_server=True)
     async def reddit(self, sub):
         """pulls a picture from reddit."""
-        try:
-            #r = praw.Reddit(user_agent="Get top wallpaper from /r/{subreddit} by /u/ssimunic".format(subreddit=sub))
-            #r = praw.Reddit(client_id='W0qqIJPsyqrKbA', client_secret='D8KHLMo62Nu8Q1wJMJRtq-FBD18', password='pass', user_agent='testscript by /u/fakebot3', username='Doodleman360_')
-            r = praw.Reddit(client_id='W0qqIJPsyqrKbA', client_secret='D8KHLMo62Nu8Q1wJMJRtq-FBD18', user_agent='testscript by /u/Doodleman360_')
-            
-            #r.subreddit(sub).id
-            for submission in reddit.subreddit('FRC').hot(limit=10):
-                await self.bot.say(submission.title)
-            await self.bot.say(r.user.me())
-        except prawcore.exceptions.Redirect:
-            await self.bot.say("sub does not exist")
-
+        #r = praw.Reddit(user_agent="Get top wallpaper from /r/{subreddit} by /u/ssimunic".format(subreddit=sub))
+        #r = praw.Reddit(client_id='W0qqIJPsyqrKbA', client_secret='D8KHLMo62Nu8Q1wJMJRtq-FBD18', password='pass', user_agent='testscript by /u/fakebot3', username='Doodleman360_')
+        r = praw.Reddit(client_id='W0qqIJPsyqrKbA', client_secret='D8KHLMo62Nu8Q1wJMJRtq-FBD18', user_agent='testscript by /u/Doodleman360_')
+        
+        #r.subreddit(sub).id
+        for submission in r.subreddit(sub).hot(limit=10):
+            await self.bot.say(submission.title)
+        await self.bot.say(r.user.me())
 def setup(bot):
     bot.add_cog(reddit(bot))
 
