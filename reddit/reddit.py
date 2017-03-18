158d1d31272787db73767c3f89948db61b75431e
Doodleman360 <julislo123@gmail.com>
2017-03-17 22:02:42 -0600

    testing

--- reddit/reddit.py
+++ reddit/reddit.py
@@ -12,19 +12,51 @@ class reddit:
     
     @commands.command(pass_context=True)
     @checks.admin_or_permissions(manage_server=True)
-    async def reddit(self, ctx, format_msg):
-        """pulls a picture from reddit."""
+    async def randR(self):
+        """pulls a random nsfw picture from reddit."""
         #r = praw.Reddit(user_agent="Get top wallpaper from /r/{subreddit} by /u/ssimunic".format(subreddit=sub))
         #r = praw.Reddit(client_id='W0qqIJPsyqrKbA', client_secret='D8KHLMo62Nu8Q1wJMJRtq-FBD18', password='pass', user_agent='testscript by /u/fakebot3', username='Doodleman360_')
         r = praw.Reddit(client_id='W0qqIJPsyqrKbA', client_secret='D8KHLMo62Nu8Q1wJMJRtq-FBD18', user_agent='testscript by /u/Doodleman360_')
         
         #r.subreddit(sub).id
         done = False
-        while not done
-            for submission in r.subreddit(format_msg).new(limit=1):
+        while not done:
+            for submission in r.subreddit("randnsfw").new(limit=1):
                 if "i.imgur.com/" in submission.url:
                     await self.bot.say(submission.url)
                     done = True
+
+    @commands.command(pass_context=True)
+    @checks.admin_or_permissions(manage_server=True)
+    async def redditNew(self, ctx, format_msg):
+        """pulls a new post from reddit."""
+        #r = praw.Reddit(user_agent="Get top wallpaper from /r/{subreddit} by /u/ssimunic".format(subreddit=sub))
+        #r = praw.Reddit(client_id='W0qqIJPsyqrKbA', client_secret='D8KHLMo62Nu8Q1wJMJRtq-FBD18', password='pass', user_agent='testscript by /u/fakebot3', username='Doodleman360_')
+        r = praw.Reddit(client_id='W0qqIJPsyqrKbA', client_secret='D8KHLMo62Nu8Q1wJMJRtq-FBD18', user_agent='testscript by /u/Doodleman360_')
+        for submission in r.subreddit(format_msg).new(limit=1):
+            await self.bot.say(submission.url)
+
+    @commands.command(pass_context=True)
+    @checks.admin_or_permissions(manage_server=True)
+    async def redditHot(self, ctx, format_msg):
+        """pulls a new post from reddit."""
+        #r = praw.Reddit(user_agent="Get top wallpaper from /r/{subreddit} by /u/ssimunic".format(subreddit=sub))
+        #r = praw.Reddit(client_id='W0qqIJPsyqrKbA', client_secret='D8KHLMo62Nu8Q1wJMJRtq-FBD18', password='pass', user_agent='testscript by /u/fakebot3', username='Doodleman360_')
+        r = praw.Reddit(client_id='W0qqIJPsyqrKbA', client_secret='D8KHLMo62Nu8Q1wJMJRtq-FBD18', user_agent='testscript by /u/Doodleman360_')
+        for submission in r.subreddit(format_msg).hot(limit=1):
+            await self.bot.say(submission.url)
+
+    @commands.command(pass_context=True)
+    @checks.admin_or_permissions(manage_server=True)
+    async def redditNew(self, ctx, format_msg):
+        """pulls the top post from a subreddit."""
+        #r = praw.Reddit(user_agent="Get top wallpaper from /r/{subreddit} by /u/ssimunic".format(subreddit=sub))
+        #r = praw.Reddit(client_id='W0qqIJPsyqrKbA', client_secret='D8KHLMo62Nu8Q1wJMJRtq-FBD18', password='pass', user_agent='testscript by /u/fakebot3', username='Doodleman360_')
+        r = praw.Reddit(client_id='W0qqIJPsyqrKbA', client_secret='D8KHLMo62Nu8Q1wJMJRtq-FBD18', user_agent='testscript by /u/Doodleman360_')
+        for submission in r.subreddit(format_msg).top("all"):
+            await self.bot.say(submission.url)
+
+
 def setup(bot):
     bot.add_cog(reddit(bot))
 
