60456e0f997a76c2ac17e8a9f2cfe999090746d9
Doodleman360 <julislo123@gmail.com>
2017-03-17 20:04:34 -0600

    testing

--- reddit/reddit.py
+++ reddit/reddit.py
@@ -19,9 +19,8 @@ class reddit:
         r = praw.Reddit(client_id='W0qqIJPsyqrKbA', client_secret='D8KHLMo62Nu8Q1wJMJRtq-FBD18', user_agent='testscript by /u/Doodleman360_')
         
         #r.subreddit(sub).id
-        for submission in r.subreddit(format_msg).hot(limit=10):
-            await self.bot.say(submission.title)
-        await self.bot.say(r.user.me())
+        for submission in r.subreddit(format_msg).hot(limit=2):
+            await self.bot.say(submission)
 def setup(bot):
     bot.add_cog(reddit(bot))
 
