e74800e259389c48d921b10bbc9b512ec38379da
Doodleman360 <julislo123@gmail.com>
2017-03-17 20:00:26 -0600

    testing

--- reddit/reddit.py
+++ reddit/reddit.py
@@ -12,14 +12,14 @@ class reddit:
     
     @commands.command(pass_context=True)
     @checks.admin_or_permissions(manage_server=True)
-    async def reddit(self, sub):
+    async def reddit(self, format_msg):
         """pulls a picture from reddit."""
         #r = praw.Reddit(user_agent="Get top wallpaper from /r/{subreddit} by /u/ssimunic".format(subreddit=sub))
         #r = praw.Reddit(client_id='W0qqIJPsyqrKbA', client_secret='D8KHLMo62Nu8Q1wJMJRtq-FBD18', password='pass', user_agent='testscript by /u/fakebot3', username='Doodleman360_')
         r = praw.Reddit(client_id='W0qqIJPsyqrKbA', client_secret='D8KHLMo62Nu8Q1wJMJRtq-FBD18', user_agent='testscript by /u/Doodleman360_')
         
         #r.subreddit(sub).id
-        for submission in r.subreddit(sub).hot(limit=10):
+        for submission in r.subreddit(format_msg).hot(limit=10):
             await self.bot.say(submission.title)
         await self.bot.say(r.user.me())
 def setup(bot):
